b817a837f52249404bd382098acc297d407adfc5
jiapeixin <18611631617@163.com>
2015-12-10 18:22:19 +0800

    单例

--- ScanQrcode/ScanSetting.h
+++ ScanQrcode/ScanSetting.h
@@ -4,13 +4,14 @@
 //
 //  Created by jiapeixin on 15/12/10.
 //  Copyright © 2015年 Gencare_Jia. All rights reserved.
-//
+// 单例
 
 #import <Foundation/Foundation.h>
 
 @interface ScanSetting : NSObject
 @property (nonatomic,assign)float sizeX;//大小
 @property (nonatomic,assign)float space;//向下偏移
+
 
 +(ScanSetting *)getsetting;
 
